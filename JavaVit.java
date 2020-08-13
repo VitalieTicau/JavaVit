64e2e2ee67527fb63fc84884adabb88541da6346
Vitalie Ticau <vitaliesticau@gmail.com>
2020-08-13 14:03:21 +0300

    mesaj1

---
+++ JavaVit.java
@@ -0,0 +1,7 @@
+public class JavaVit
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Java programare!");
+	}
+}
---
+++ SalutVit.java
@@ -0,0 +1,7 @@
+public class SalutVit
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Salut!");
+	}
+}
